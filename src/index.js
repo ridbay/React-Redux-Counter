1a26a91cef8ff41cb69724885f42620d6dac8d9d
Ridwan <balogunridwan@gmail.com>
2019-09-10 17:34:53 +0100

    added store, and dispatched actions

--- src/index.js
+++ src/index.js
@@ -2,11 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux'
+import { stat } from 'fs';
+
+
+
+const initialState = {
+    count: 0
+}
+const reducer = (state = initialState, action) => {
+  console.log("reducer", state, action);
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+    case "RESET":
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+console.log(store)
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'INCREMENT'});
+store.dispatch({type: 'DECREMENT'});
+store.dispatch({type: 'RESET'});
+store.dispatch({type: 'RESET'});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
